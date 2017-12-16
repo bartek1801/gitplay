3c320febc40d9dc071e626b3159feb6615532637
Bartlomiej <lechbartlomiej1801@gmail.com>
2017-12-16 12:25:29 +0100

    inne zmiany w pliku Hello

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 class Hello{
 		public static void main(String[] args){
-			System.out.println("Hello!!!");
+			System.out.println("Hello bar bar!!!");
+		}
+		
+		public void bar(){
+			System.out.println("bar bar");
 		}
 }
