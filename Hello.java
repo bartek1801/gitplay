bb83080f0bbcdc1b0297cd031d30e28ef19b8d91
Bartlomiej <lechbartlomiej1801@gmail.com>
2017-12-16 13:38:53 +0100

    commit from intelliJ IDEA

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class Hello{
 		public static void main(String[] args){
 			System.out.println("Hello hello!!!");
 			System.out.println("Hello bar bar!!!");
+			System.out.println("Ale jaja");
 		}
 		
 		public void bar(){
