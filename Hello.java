e32b2d68dae69bdcb6950637e3b51751c2659742
Bartlomiej <lechbartlomiej1801@gmail.com>
2017-12-16 13:03:55 +0100

    lepsza zmiana

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
 		public static void main(String[] args){
-			System.out.println("Hello hello!!!");
+			System.out.println("Hello hello and welcome!!!");
 		}
 		
 		public void foo(){
