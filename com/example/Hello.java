9dd4ba3749c4ee6060084b159c11ae59434c7cd1
Bartlomiej <lechbartlomiej1801@gmail.com>
2017-12-16 13:53:59 +0100

    zmiana w klasie Hello

--- com/example/Hello.java
+++ com/example/Hello.java
@@ -6,13 +6,18 @@ class Hello{
 			System.out.println("com.example.Hello bar bar!!!");
 			System.out.println("Ale jaja");
 		}
-		
-		public void bar(){
+
+	public Hello() {
+	}
+
+	public void bar(){
 			System.out.println("bar bar");
 		}
 
 		
 		public void foo(){
+			System.out.println("Moje inne zmiany");
+			System.out.println("hello");
 			System.out.println("Ale fajny GIT");
 		}
 		
