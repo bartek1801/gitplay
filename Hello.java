b44e643995739e611306c8f953cd2295d78cf1eb
Bartlomiej <lechbartlomiej1801@gmail.com>
2017-12-16 13:46:23 +0100

    jakas zmiana

--- Hello.java
+++ Hello.java
@@ -17,12 +17,13 @@ class Hello{
 			System.out.println("Moje inne zmiany");
 			System.out.println("hello");
 		}
-		
+
 		public void boo(){
 			System.out.println("test text");
 		}
 
 		public void method(){
 			System.out.println("important method");
+			System.out.println("Ale fajny GIT");
 		}
 }
