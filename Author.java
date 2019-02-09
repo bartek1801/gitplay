d94fb28636b76e1259de140da397673c21f8346a
Bartlomiej <lechbartlomiej1801@gmail.com>
2019-02-09 23:28:20 +0100

    added pseudonim field to Author

--- Author.java
+++ Author.java
@@ -2,13 +2,16 @@ public class Author {
 
     private String firstName;
     private String lastName;
+    private String pseudonim;
+
 
     public Author() {
     }
 
-    public Author(String firstName, String lastName) {
+    public Author(String firstName, String lastName, String pseudonim) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.pseudonim = pseudonim;
     }
 
     public String getFirstName() {
@@ -25,5 +28,13 @@ public class Author {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getPseudonim() {
+        return pseudonim;
+    }
+
+    public void setPseudonim(String pseudonim) {
+        this.pseudonim = pseudonim;
     }
 }
